6dfe2f03674844445a00a4b4d4e4cf0ee2ccec20
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2013-03-15 16:32:27 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 amd-mode
 ========
 
-AMD minor mode for Emacs to easily work with AMD dependencies.
+AMD minor mode for Emacs to easily work with AMD dependencies. Some ideas were kindly stolen from requirejs-mode.
+
+Envisioned features:
+ * Add dependency by hand (specify id interactively).
+ * Allow user to specify AMD libraries on disk which can be used in selection and where the id-namespace is handled corectly. That means selecting '~/dojo/dijit/_Widget.js' will result in the id 'dijit/_Widget' when the user sepcified 'dijit -> ~/dojo/dijit'.
+ * Select dependencies from files. Allow configuration of folders to look and handle prefixes correctly.
+ * Invoke dependency suggestion when point is on an identifier.
+ * Remove dependency when point is on the id or on the varaible for that import.
---
+++ amd-mode.el
@@ -0,0 +1,151 @@
+(require 'cl)
+
+(setq amd--header-regexp "^define\\s-*(\\s-*\\(\\[[^]]*\\]\\)\\s-*,\\s-*function\\s-*\\(([^)]*)\\)\\s-*{")
+
+(defun cadr (l)
+  (car (cdr l)))
+
+(defun amd--strip-parens (string)
+  (substring string 1 -1))
+
+(defun amd--trim (string)
+  (replace-regexp-in-string "\\`[ \t\n]*" ""
+                            (replace-regexp-in-string "[ \t\n]*\\'" "" string)))
+
+(defun amd--parse-comma-list (string)
+  (mapcar (lambda (s) (amd--trim s))
+          (split-string (amd--trim (amd--strip-parens (amd--trim string))) ",")))
+
+(defun amd--make-header (ids vars id-region var-region)
+  (if (equal (length ids) (length vars))
+    (let ((theTable (make-hash-table :test 'equal)))
+      (progn (mapcar* (lambda (id var) (puthash id var theTable))
+                      ids vars)
+             (list theTable id-region var-region)))
+    (identity nil)))
+
+(defun amd--header-table (header)
+  (car header))
+
+(defun amd--header-id-region (header)
+  (car (cdr header)))
+
+(defun amd--header-var-region (header)
+  (car (cdr (cdr header))))
+
+(defun amd--make-region (start end)
+  (list start end))
+
+(defun amd--region-start (region)
+  (car region))
+
+(defun amd--region-end (region)
+  (cadr region))
+
+(defun amd--region-length (region)
+  (- (amd--region-end region) (amd--region-start region)))
+
+(defun amd--delete-region (region)
+  (delete-region (amd--region-start region) (amd--region-end region)))
+
+(defun amd--header-ids (header)
+  (let ((table (amd--header-table header))
+        (result ()))
+    (maphash (lambda (kk vv)
+               (setq result (cons kk result)))
+             table)
+    (sort result 'string<)))
+
+(defun amd--header-vars (header)
+  (mapcar (lambda (id) (gethash id (amd--header-table header)))
+          (amd--header-ids header)))
+
+(defun amd--header-add (id var header)
+  (puthash id var (amd--header-table header)))
+
+(defun amd--header-var-by-id (id header)
+  (gethash id (amd--header-table header)))
+
+(defun amd--header-id-by-var (var header)
+  (let (id)
+    (maphash (lambda (kk vv)
+               (if (equals vv var)
+                 (setq id kk)))
+             (amd--header-table header))
+    (identity id)))
+
+(defun amd--header-del-id (id header)
+  (remhash id (amd--header-table header)))
+
+(defun amd--header-del-var (var header)
+  (remhash (amd--header-id-by-var var header)
+           (amd--header-table header)))
+
+(defun amd--write-header (header)
+  (print header)
+  (let ((id-region (amd--header-id-region header))
+        (var-region (amd--header-var-region header)))
+    (let ((new-id-start (amd--region-start id-region))
+          (new-var-start (- (amd--region-start var-region)
+                            (amd--region-length id-region))))
+      (amd--delete-region var-region)
+      (amd--delete-region id-region)
+      (goto-char new-var-start)
+      (insert (concat "(" (mapconcat 'identity
+                                       (amd--header-vars header)
+                                       ",") ")"))
+      (goto-char new-id-start)
+      (insert (concat "[\n" (mapconcat (lambda (id) (concat "    " id))
+                                       (amd--header-ids header)
+                                       ",\n") "\n]")))))
+
+(defun amd--find-header ()
+  "Find the AMD header and parse it"
+  (beginning-of-buffer)
+  (if (search-forward-regexp amd--header-regexp nil t)
+    (let ((raw-ids (match-string 1))
+          (raw-vars (match-string 2)) 
+          (id-region (list (match-beginning 1) (match-end 1)))
+          (var-region (list (match-beginning 2) (match-end 2))))
+      (let ((ids (amd--parse-comma-list raw-ids))
+            (vars (amd--parse-comma-list raw-vars)))
+        (amd--make-header ids vars id-region var-region)))))
+
+(defun amd--write-empty-header ()
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (beginning-of-buffer)
+  (insert "define([],function(){\n")
+  (end-of-buffer)
+  (insert "});"))
+
+(defun amd--find-or-create-header ()
+  (let ((header (amd--find-header)))
+    (if header
+      (identity header)
+      (progn (amd--write-empty-header)
+             (amd--find-header)))))
+
+;;; Commands
+
+(define-minor-mode amd-mode "AMD mode
+
+This mode is intended to provide easier management of
+dependencies in an AMD style Javascript module."
+
+  nil " AMD" nil)
+
+(defun amd-init ()
+  "Make current buffer into an AMD module."
+  (interactive)
+  (if (amd--find-header)
+    (message "AMD header already exists.")
+    (amd--write-empty-header)))
+
+(defun amd-add ()
+  "Add AMD depenency to header"
+  (interactive)
+  (let ((header (amd--find-or-create-header)))
+    (amd--write-header header)))
+
+(provide 'amd-mode)
