7b59f8af87a285d8657baf68b2df7c22331149ad
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2013-03-28 19:14:46 +0100

    Refactored amd-mode, depends now on external js-pkg and semver.

--- amd-header.el
+++ amd-header.el
@@ -1,156 +1,154 @@
-(require 'cl)
-(require 'amd-util)
+(require 'dash)
+(require 's)
+(require 'amd-dep)
 
-;;; Reading and writing the header
+(setq amd-header--re
+  (let*
+      ((g< "\\(?:")
+       (>g "\\)")
+       (string (concat g< "\"[^\"]*\"\\|'[^']*'" >g))
+       (identifier "[_$[:alnum:]]+")
+       (ws* "[\n[:space:]]*")
+       (strings? (concat g< string
+                         g< ws* "," ws* string >g "*" >g "?"))
+       (identifiers? (concat g< identifier
+                             g< ws* "," ws* identifier >g "*" >g "?")))
+    (concat "^define" ws* "(" ws* "\\(\\[" ws* strings? ws* "\\]\\)" ws*
+            "," ws* "function" ws* "\\((" ws* identifiers? ws* ")\\)" ws*
+            "{")))
 
-(defvar amd-header-re
-  (let* ((g< "\\(?:")
-         (>g "\\)")
-         (string (concat g< "\"[^\"]*\"\\|'[^']*'" >g))
-         (identifier "[_$[:alnum:]]+")
-         (ws* "[\n[:space:]]*")
-         (strings? (concat g< string
-                           g< ws* "," ws* string >g "*" >g "?"))
-         (identifiers? (concat g< identifier
-                               g< ws* "," ws* identifier >g "*" >g "?")))
-    (concat "^define" ws* "(" ws* "\\(\\[" ws* strings? ws* "\\]\\)" ws* ","
-            ws* "function" ws* "\\((" ws* identifiers? ws* ")\\)" ws* "{")))
+(defun amd-header-read ()
+  "Read header from current buffer or nil."
+  (let ((match (amd-header--match)))
+    (when match
+      (amd-header-create (nth 1 match)
+                         (nth 2 match)))))
 
-(defun amd-find-header ()
-  "Find the AMD header and parse it"
-  (goto-char (point-min))
-  (if (search-forward-regexp amd-header-re nil t)
-      (let ((raw-ids (match-string 1))
-            (raw-vars (match-string 2)) 
-            (id-region (list (match-beginning 1) (match-end 1)))
-            (var-region (list (match-beginning 2) (match-end 2))))
-        (let ((ids (amd-parse-id-list raw-ids))
-              (vars (amd-parse-var-list raw-vars)))
-          (amd-make-header ids vars id-region var-region)))))
+(defun amd-header-write (header &optional add-if-new)
+  (let ((match (amd-header--match)))
+    (when (or match
+              add-if-new)
+      (if match
+          (delete-region (car (nth 0 match))
+                         (cdr (nth 0 match)))
+        ; how can we indent the existing code?
+        (goto-char (point-max))
+        (insert "\n});\n")
+        (goto-char (point-min)))
+      (insert (amd-header--format header)))))
 
-(defun amd-parse-id-list (string)
-  (mapcar (lambda (s) (amd-strip-parens s))
-          (amd-split-list string)))
-
-(defun amd-parse-var-list (string)
-  (amd-split-list string))
-
-(defun amd-split-list (string)
-  (mapcar (lambda (s) (amd-trim s))
-          (split-string (amd-strip-parens string) "," t)))
-
-(defun amd-strip-parens (string)
-  (amd-trim (substring (amd-trim string) 1 -1)))
-
-(defun amd-trim (string)
-  (let ((ws* "[\n[:space:]]*"))
-    (replace-regexp-in-string ws* ""
-                              (replace-regexp-in-string ws* "" string))))
-
-(defun amd-write-header (header)
-  (let ((id-region (amd-header-id-region header))
-        (var-region (amd-header-var-region header)))
-    (let ((new-id-start (amd-region-start id-region))
-          (new-var-start (- (amd-region-start var-region)
-                            (amd-region-length id-region))))
-      (amd-delete-region var-region)
-      (amd-delete-region id-region)
-      (goto-char new-var-start)
-      (insert (concat "(" (mapconcat 'identity
-                                       (amd-header-vars header)
-                                       ", ") ")"))
-      (goto-char new-id-start)
-      (insert (concat "[\n" (mapconcat (lambda (id) (concat "    \"" id "\""))
-                                       (amd-header-ids header)
-                                       ",\n") "\n]")))))
-
-(defun amd-write-empty-header ()
-  (goto-char (point-min))
-  (insert "define([],function(){\n")
-  (goto-char (point-max))
-  (insert "\n});"))
-
-(defun amd-find-or-create-header ()
-  (let ((header (amd-find-header)))
-    (if header
-        header
-      (progn (amd-write-empty-header)
-             (amd-find-header)))))
-
-;;; Header data structure
-
-(defun amd-make-header (ids vars id-region var-region)
-  (if (equal (length ids) (length vars))
-      (let ((theTable (make-hash-table :test 'equal)))
-        (progn (mapcar* (lambda (id var) (puthash id var theTable))
-                        ids vars)
-               (list theTable id-region var-region)))
-    (identity nil)))
-
-(defun amd-header-table (header)
-  (car header))
-
-(defun amd-header-id-region (header)
-  (car (cdr header)))
-
-(defun amd-header-var-region (header)
-  (car (cdr (cdr header))))
-
-(defun amd-header-ids (header)
-  "Return a sorted list of all module ids."
-  (sort (hash-table-keys (amd-header-table header))
-        'string<))
-
-(defun amd-header-vars (header)
-  "Return a list of all variable names in the same order as amd-header-ids."
-  (hash-table-values (amd-header-table header)
-                     (amd-header-ids header)))
+(defun amd-header-create (&optional deps vars)
+  (setq deps (-map 'amd-dep-parse deps))
+  (let ((h (list 'amd-header nil)))
+    (amd-header--set-depvars (-zip deps vars) h)
+    h))
 
 (defun amd-header-add (dep var header)
-  "Add a module ID and return the new or already existing variable name."
-  (let* ((table (amd-header-table header))
-         (curvar (gethash dep table)))
-    (if curvar
-        curvar
-      (let ((newvar (amd-header-unique-var (amd-header-safe-var var) header)))
-        (puthash dep newvar table)
-        newvar))))
+  (setq dep (amd-dep-parse dep))
+  (unless (amd-header-dep-by-var dep header)
+    (let ((unique-var (amd-header--unique-var
+                       (amd-header--safe-var var)
+                       (amd-header-vars header))))
+      (amd-header--set-depvars (cons (cons dep unique-var)
+                                     (amd-header-depvars header))
+                               header)
+      unique-var)))
 
-(defun amd-header-safe-var (var)
-  (replace-regexp-in-string "[^[:alnum:]_$]+" "" var))
-
-(defun amd-header-unique-var (var header)
-  (unique-string var (amd-header-vars header)))
-
-(defun amd-header-var-by-id (id header)
-  "Return the variable name for a module id or nil."
-  (gethash id (amd-header-table header)))
-
-(defun amd-header-id-by-var (var header)
-  "Return the variable name for a module id or nil."
-  (hash-table-find var (amd-header-table header)))
-
-(defun amd-header-del-id (id header)
-  (remhash id (amd-header-table header)))
+(defun amd-header-del-dep (dep header)
+  (setq dep (amd-dep-parse dep))
+  (amd-header--set-depvars
+   (-remove (lambda (dv)
+              (equal (car dv) dep))
+            (amd-header-depvars header))
+   header))
 
 (defun amd-header-del-var (var header)
-  (remhash (amd-header-id-by-var var header)
-           (amd-header-table header)))
+  (amd-header--set-depvars
+   (-remove (lambda (v)
+              (equal (cdr v) var))
+            (amd-header-depvars header))
+   header))
 
-;;; Region data structure
+(defun amd-header--set-depvars (depvars header)
+  "Replace existing depvars with the given one."
+  (setcar (nthcdr 1 header) depvars))
 
-(defun amd-make-region (start end)
-  (list start end))
+(defun amd-header-depvars (header)
+  "Return alist of (dep . var)"
+  (nth 1 header))
 
-(defun amd-region-start (region)
-  (car region))
+(defun amd-header-deps (header)
+  "Return list of deps. Order corresponds to amd-header-vars."
+  (-map 'car (amd-header-depvars header)))
 
-(defun amd-region-end (region)
-  (cadr region))
+(defun amd-header-vars (header)
+  "Return list of vars. Order corresponds to amd-header-deps."
+  (-map 'cdr (amd-header-depvars header)))
 
-(defun amd-region-length (region)
-  (- (amd-region-end region) (amd-region-start region)))
+(defun amd-header-var-by-dep (dep header)
+  "Return the var belonging to this dep or nil."
+  (setq dep (amd-dep-parse dep))
+  (cdr (assoc dep (amd-header-depvars header))))
 
-(defun amd-delete-region (region)
-  (delete-region (amd-region-start region) (amd-region-end region)))
+(defun amd-header-dep-by-var (var header)
+  "Return the dep belonging to this var or nil."
+  (car (rassoc var (amd-header-depvars header))))
+
+(defun amd-header--match ()
+  "Find the AMD header and parse it"
+  (goto-char (point-min))
+  (when (search-forward-regexp amd-header--re nil t)
+    (let ((start (match-beginning 0))
+          (end (match-end 0))
+          (raw-deps (match-string 1))
+          (raw-vars (match-string 2)))
+      (let ((deps (amd-header--parse-deps raw-deps))
+            (vars (amd-header--parse-vars raw-vars)))
+        (list (cons start end) deps vars)))))
+
+(defun amd-header--parse-deps (string)
+  (-map (lambda (s)
+          (amd-dep-parse (amd-header--strip-parens s)))
+        (amd-header--parse-list string)))
+
+(defun amd-header--parse-vars (string)
+  (amd-header--parse-list string))
+
+(defun amd-header--parse-list (string)
+  (-map (lambda (s) (s-trim s))
+        (s-split "," (amd-header--strip-parens string) t)))
+
+(defun amd-header--strip-parens (string)
+  (s-trim (substring (s-trim string) 1 -1)))
+
+(defun amd-header--format (header)
+  (let ((depvars (amd-header-depvars header)))
+    (setq depvars (-map (lambda (depvar)
+                          (cons (amd-dep-format (car depvar))
+                                (cdr depvar)))
+                          depvars))
+    (setq depvars (sort depvars
+                        (lambda (obj wrt)
+                          (string< (car obj) (car wrt)))))
+    (concat "define([\n"
+            (s-join ",\n" (mapcar (lambda (s)
+                                    (concat "    \"" s "\""))
+                                  (-map 'car depvars)))
+            "\n],function("
+            (s-join ", " (-map 'cdr depvars))
+            ") {\n")))
+
+(defun amd-header--safe-var (var)
+  "Return the string with unsafe characters removed."
+  (s-replace "[^[:alnum:]_$]+" "" var))
+
+(defun amd-header--unique-var (string strings)
+  "Make string unique so that it does not exists in strings by appending a counter."
+  (let ((counter 1)
+        (unique-string string))
+    (while (member unique-string strings)
+      (setq counter (1+ counter))
+      (setq unique-string (format (concat string "%d") counter)))
+    unique-string))
+
 (provide 'amd-header)
--- amd-mode.el
+++ amd-mode.el
@@ -1,11 +1,11 @@
 ;;; Requires
 
+(require 'dash)
 (require 'ido)
+(require 'thingatpt)
 
 (require 'amd-header)
 (require 'amd-dep)
-(require 'amd-package)
-(require 'amd-util)
 
 ;;; Commands
 
@@ -13,7 +13,7 @@
 (define-key amd-key-map (kbd "C-c a i") 'amd-init)
 (define-key amd-key-map (kbd "C-c a d") 'amd-add-dep)
 (define-key amd-key-map (kbd "C-c a f") 'amd-add-file)
-(define-key amd-key-map (kbd "C-c a p") 'amd-add-pkg)
+(define-key amd-key-map (kbd "C-c a p") 'amd-register-pkg)
 (define-key amd-key-map (kbd "C-c a g") 'amd-goto)
 
 (define-minor-mode amd-mode "AMD mode
@@ -27,9 +27,9 @@ dependencies in an AMD style Javascript module."
   "Make current buffer into an AMD module."
   (interactive)
   (save-excursion
-    (if (not (amd-find-header))
-        (amd-write-empty-header)
-      (message "AMD header already exists."))))
+    (if (amd-header-read)
+        (message "AMD header already exists.")
+      (amd-header-write (amd-header-create)))))
 
 (defun amd-add-dep ()
   "Add AMD dependency to header"
@@ -39,79 +39,88 @@ dependencies in an AMD style Javascript module."
            (dep (amd-dep-parse depstr)))
       (if (not dep)
           (message "Dependency %s has invalid format." depstr)
-        (amd-write-dep-to-header dep)))))
+        (amd--add-dep-to-header dep)))))
 
 (defun amd-add-file ()
-  "Add AMD dependency to header by file"
+  "Add AMD dependency to header by file."
   (interactive)
   (save-excursion
     (let ((new-file (ido-read-file-name "Select dependency file: ")))
-      (when (if (not (file-exists-p new-file))
-                (when (y-or-n-p "File does not exist, create? ")
-                  (with-current-buffer (find-file-other-window new-file)
-                    (save-buffer)
-                    (set-buffer-modified-p t)
-                    t))
-              t)
-        (let* ((deps (amd-deps-from-file new-file))
-               (count (length deps))
-               (dep (cond ((= 0 count)
-                           (message "No options found for this file.")
-                           nil)
-                          ((= 1 count)
-                           (car deps))
-                          (t
-                           (let ((depstr
-                                  (ido-completing-read
-                                   "Select import: "
-                                   (mapcar 'amd-dep-format deps))))
-                             (if (and depstr (not (equal "" depstr)))
-                                 (amd-dep-parse depstr)))))))
-          (if (not dep)
-              (message "Failed to add dependency.")
-            (amd-write-dep-to-header dep)))))))
-
-(defun amd-write-dep-to-header (dep)
-  (let* ((var (or (amd-dep-to-var dep)
-                  (read-string "No auto-name, specify variable name: ")))
-         (header (amd-find-or-create-header))
-         (final-var (amd-header-add (amd-dep-format dep) var header)))
-    (amd-write-header header)
-    (message "Dependency %s available as %s" (amd-dep-format dep) final-var)))
-    
-(defun amd-add-pkg ()
-  "Add a package from a directory"
-  (interactive)
-  (let ((directory (ido-read-directory-name
-                    "Select package directory: ")))
-    (when (if (not (file-exists-p directory))
-              (when (y-or-n-p "Directory doesn't exist, create?")
-                (make-directory directory t)
-                t)
-            t)
-      (let ((name (amd-package-read-json directory)))
-        (when (not name)
-          (setq name (read-string "No package name found, specify: "
-                                  (or (amd-package-name directory)
-                                      (amd-package-guess-name directory))))
-          (when (y-or-n-p "No package.json found, create? ")
-            (amd-package-write-json directory name)))
-        (amd-package-add name directory)))))
+      (let* ((deps (amd-deps-from-file new-file))
+             (count (length deps))
+             (dep (cond ((= 0 count)
+                         (message "No way to add file. Maybe file is not part of a package?")
+                         nil)
+                        ((= 1 count)
+                         (car deps))
+                        (t
+                         (let ((depstr
+                                (ido-completing-read
+                                 "Select import: "
+                                 (-map 'amd-dep-format deps))))
+                           (if (and depstr (not (equal "" depstr)))
+                               (amd-dep-parse depstr)))))))
+        (if (not dep)
+            (message "Failed to add dependency.")
+          (amd--add-dep-to-header dep)
+          (unless (file-exists-p new-file)
+            (when (y-or-n-p "File does not exist, create? ")
+              (with-current-buffer (find-file-other-window new-file)
+                (amd-header-write (amd-header-create) t)
+                (save-buffer)))))))))
 
 (defun amd-goto ()
   "Open one of the dependencies"
   (interactive)
-  (let ((header (amd-find-header)))
-    (if header
-        (let* ((depstrs (amd-header-ids header))
-               (depstr (ido-completing-read "Open dependency: " depstrs))
-               (dep (amd-dep-parse depstr)))
-           (if dep
-               (let ((file (amd-dep-to-file dep)))
-                 (if file
-                     (find-file-other-window file)
-                   (message "No file found for dependecy %s." depstr)))
-             (message "Failed to parse dependency %s." depstr)))
-      (message "No AMD header found."))))
+  (let ((header (amd-header-read)))
+    (if (not header)
+        (message "No AMD header found.")
+      (let* ((deps (amd-header-deps header))
+             (depstrs (-map 'amd-dep-format deps))
+             (depstr (ido-completing-read
+                      "Open dependency: " depstrs))
+             (dep (amd-dep-parse depstr)))
+        (if (not dep)
+            (message "Failed to parse dependency %s." depstr)
+          (let ((files (amd-dep-to-files dep))
+                (count (length files)))
+            (cond ((= count 0)
+                   (message "No file found for dependecy %s." depstr))
+                  ((= count 1)
+                   (find-file-other-window (nth 0 files)))
+                  (t
+                   (let ((file (ido-completing-read
+                                "Select from options: " files)))
+                     (when file
+                       (find-file-other-window (nth 0 files))))))))))))
 
+(defun amd-register-pkg ()
+  "Add a package from a directory"
+  (interactive)
+  (let ((directory (ido-read-directory-name
+                    "Select package directory: ")))
+    (unless (js-pkg-info-by-file directory)
+      (when (y-or-n-p "Package doesn't exist, create?")
+        (unless (file-exists-p directory)
+          (make-directory directory t))
+        (let ((name (read-string "Give package name: "
+                                 (file-name-nondirectory
+                                  (directory-file-name directory))))
+              (version (semver-parse
+                        (read-string "Give package version: " "0.0.0"))))
+          (when (and name version)
+            (js-pkg-info-write
+             (js-pkg-info-create name version directory))))))))
+
+(defun amd--add-dep-to-header (dep)
+  (let* ((var (or (amd-dep-to-var dep)
+                  (read-string "No auto-name, specify variable name: ")))
+         (header (or (amd-header-read)
+                     (amd-header-create)))
+         (final-var (amd-header-add dep var header)))
+    (when final-var
+      (amd-header-write header t)
+      (message "Dependency '%s' added as variable '%s'"
+               (amd-dep-format dep) final-var))))
+    
 (provide 'amd-mode)
